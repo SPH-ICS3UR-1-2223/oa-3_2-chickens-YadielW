580951f1ac435d121daccf70e8c056d8d545c6bb
YadielW <yadiel.wondimu@stu.ocsb.ca>
2022-09-23 14:36:49 -0400

    Used ints

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,6 +1,18 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
+    	int chickenCount = 3;
+    	int eggsPerChicken = 5;
+    	int totalEggs = 0;
+    	
+    	//Monday
+    	totalEggs = chickenCount * eggsPerChicken;
+    	//Tuesday
+    	chickenCount=chickenCount + 1;
+    	totalEggs += chickenCount * eggsPerChicken;
+    	//Wednesday
+    	chickenCount/=2;
+    	totalEggs += chickenCount * eggsPerChicken;
         
         System.out.println(totalEggs);
     }   
--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,9 +1,25 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
-
+    	double dailyAverage = 0;
+    	double eggCount=0;
+    	double monthlyAverage=0;
+    	double monthlyProfit=0;
+    	//Monday
+    	eggCount=100; //collect 100 eggs
+    	//Tuesday
+    	eggCount+=121; //collect 121 eggs
+    	//Wednesday
+    	eggCount+=117; //collect 117 eggs
+    	
+    	//Calculate daily average
+    	dailyAverage=eggCount/3;
         System.out.println("Daily Average:   " +dailyAverage);
+        //Calculate monthly average
+        monthlyAverage = dailyAverage*30;
         System.out.println("Monthly Average: " +monthlyAverage);
+        //calculate monthly profit
+        monthlyProfit = monthlyAverage*0.18;
         System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
     
